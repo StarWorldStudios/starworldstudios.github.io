22974ef491fdaf8d6200afd3a918e5ff92a8006f
StarWorldStudios <76687040+StarWorldStudios@users.noreply.github.com>
2021-09-19 17:58:18 +0800

    Update language.js

--- zh_CN/script/language.js
+++ zh_CN/script/language.js
@@ -1,3 +1,3 @@
-document.write('<script src="https://starworldstudios.github.io/en_US/js/vue.js"></script>')
+document.write('<script src="https://starworld.itcraft.tk/en_US/js/vue.js"></script>')
 document.write('<script src="https://starworld.itcraft.tk/zh_CN/lib/jquery-3.5.1.js"></script>')
 document.write('<script src="https://starworld.itcraft.tk/zh_CN/lib/jquery-3.5.1.min.js"></script>')
